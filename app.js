2b2df27840eb832f714e4f2a2cd99db1172352ef
Michael-Hanley <pasaquali@gmail.com>
2019-05-30 18:33:21 -0700

    Update package.json and package-lock.json

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ const mysql = require('mysql');
 const dataSync = require('./data-sync');
 const db = require('./dbconnection');
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(require('./middlewares/users'));
 app.use(require('./controllers'));
-
-
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...')
