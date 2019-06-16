856f1cb8f1ec130e4737a6d2bd32e9d108e27e07
Michael-Hanley <pasaquali@gmail.com>
2019-06-15 17:59:18 -0700

    Include data sync import

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('./data-sync');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
